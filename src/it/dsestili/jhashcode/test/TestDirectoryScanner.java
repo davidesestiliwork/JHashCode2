a0a8e035088756f48475466faf843f375f41699d
davide <davide@davide-SVE1512H1EW>
2017-06-19 11:48:05 +0200

    copyright notice

--- src/it/dsestili/jhashcode/test/TestDirectoryScanner.java
+++ src/it/dsestili/jhashcode/test/TestDirectoryScanner.java
@@ -2,7 +2,7 @@ package it.dsestili.jhashcode.test;
 
 /*
 JHashCode a simple hash code generator
-Copyright (C) 2013-2016 Davide Sestili
+Copyright (C) 2017 Davide Sestili
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
